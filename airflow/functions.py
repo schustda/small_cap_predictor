5f3cf2035ce31cab4ae630edaa5d6694cd01a23e
schustda <douglas.schuster303@gmail.com>
2018-08-16 22:42:29 -0400

    airflow for etl

---
+++ airflow/functions.py
@@ -0,0 +1,20 @@
+def retrieve_symbol_ids():
+    pass
+
+def price_history_data():
+    pass
+
+def remove_last_combined_value():
+    pass
+
+def ihub_top_boards():
+    pass
+
+def ihub_breakout_boards():
+    pass
+
+def combine_data():
+    pass
+
+def make_predictions():
+    pass
