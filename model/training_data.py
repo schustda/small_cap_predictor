5fd1d1d739ccbd5d58959402e2550d2b01b7985a
Douglas Schuster <Douglas.Schuster@carat.com>
2018-09-09 17:25:26 +0100

    added status to training data

--- model/training_data.py
+++ model/training_data.py
@@ -54,10 +54,19 @@ class TrainingData(ModelBaseClass):
             self.conn.rollback()
 
     def insert_splits(self,splits,symbol_id=None):
+
+        self.interval_time, self.original_time = time(), time()
+
         for column,idxs in splits.items():
+            print('reseting {0}'.format(column))
             self.reset_split(column,symbol_id=symbol_id)
-            for idx in idxs:
+            print('adding splits for {0}'.format(column))
+
+            for num,idx in enumerate(idxs):
                 self.set_split(column,idx)
+
+                percent = int(num/len(idxs))*100
+                self.status_update(percent)
 
     def working_split(self,symbol_id):
         '''
@@ -67,13 +76,14 @@ class TrainingData(ModelBaseClass):
         symbol into a train test set. Will reset what was previously selected.
         '''
         df = self.get_df('get_combined_data',symbol_id=symbol_id)
+        print('got data')
 
         # only include points after a certain number of days that the
         # stock has been on the market
         df = df[self.model_params['buffer_days']:].dropna(subset=['defined_target'])
 
         train,validation = train_test_split(df.idx.tolist())
-
+        print('inserting')
         self.insert_splits({'working_train':train,
             'working_validation':validation},symbol_id)
 
@@ -84,6 +94,7 @@ class TrainingData(ModelBaseClass):
         '''
 
         idxs = self.get_idxs('working_train')
+        print('got idxs')
         train,test = train_test_split(idxs)
 
         self.insert_splits({'model_development_train':train,
@@ -113,22 +124,23 @@ if __name__ == '__main__':
 
 
     td = TrainingData(verbose=True)
+    td.model_development_split()
     # df = td.create_training_data('model_development_train')
     # df = td.create_training_data('model_development_test')
     # td.working_train_validation()
     # df = td.pull_and_transform_point(1689)
 
 
-    symbol_ids = td.get_list('symbol_ids')
-    grp1 = [x for x in symbol_ids if not x%4]
-    grp2 = [x for x in symbol_ids if not (x+1)%4]
-    grp3 = [x for x in symbol_ids if not (x+2)%4]
-    grp4 = [x for x in symbol_ids if not (x+3)%4]
-    for symbol_id in eval(argv[1]):
+    # symbol_ids = td.get_list('symbol_ids')
+    # grp1 = [x for x in symbol_ids if not x%4]
+    # grp2 = [x for x in symbol_ids if not (x+1)%4]
+    # grp3 = [x for x in symbol_ids if not (x+2)%4]
+    # grp4 = [x for x in symbol_ids if not (x+3)%4]
+    # for symbol_id in eval(argv[1]):
     # for symbol_id in symbol_ids:
-        print(symbol_id)
-        td.working_train_validation(symbol_id,reset=True)
-    td.generate_training_data()
+        # print(symbol_id)
+        # td.working_split(symbol_id)
+    # td.generate_training_data()
 
 
 
