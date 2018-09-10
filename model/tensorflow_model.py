a8f6f0c0bb11ecd825e8c0171ac26b2d3bdb1e16
schustda <douglas.schuster@carat.com>
2018-09-10 09:22:59 -0400

    added variable to remove points with a zero target

--- model/tensorflow_model.py
+++ model/tensorflow_model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from random import sample
 from model.model_base_class import ModelBaseClass
 
 
@@ -9,8 +10,14 @@ if __name__ == '__main__':
 
     x_train = mbc.X_train
     y_train = mbc.y_train
-    x_test = mbc.X_test
     y_test = mbc.y_test
+    x_test = mbc.X_test
+
+    # there is a high number of points that have
+    zeroes = set([num for num,value in enumerate(y_train) if value==0])
+    idxs_to_remove = set(sample(zeroes,int(len(zeroes)*(1-mbc.model_params['zero_target_percentage']))))
+    mask = [num not in idxs_to_remove for num in range(len(y_train))]
+    x_train = x_train[mask]
 
     model = tf.keras.models.Sequential([
       tf.keras.layers.Flatten(),
@@ -19,10 +26,8 @@ if __name__ == '__main__':
       tf.keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
     model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
+                  loss='mean_squared_error',
                   metrics=['accuracy'])
-
-    model.fit(x_train, y_train, epochs=5, verbose=1)
+    model.fit(x_train, y_train, epochs=10, verbose=1)
     model.evaluate(x_test, y_test)
-
-    
+    model.save('scp_model.h5')
