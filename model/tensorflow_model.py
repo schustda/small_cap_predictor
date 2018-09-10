c9cbcbd32d1b49203c532f41cbc3fbf62b36cf3e
schustda <douglas.schuster@carat.com>
2018-09-10 19:49:47 -0400

    added class variable of idx to training data

--- model/tensorflow_model.py
+++ model/tensorflow_model.py
@@ -10,14 +10,19 @@ if __name__ == '__main__':
 
     x_train = mbc.X_train
     y_train = mbc.y_train
+    train_idx = mbc.train_idx
     y_test = mbc.y_test
     x_test = mbc.X_test
+    test_idx = mbc.test_idx
 
     # there is a high number of points that have
     zeroes = set([num for num,value in enumerate(y_train) if value==0])
     idxs_to_remove = set(sample(zeroes,int(len(zeroes)*(1-mbc.model_params['zero_target_percentage']))))
     mask = [num not in idxs_to_remove for num in range(len(y_train))]
     x_train = x_train[mask]
+
+    model_path = 'model/scp_model.h5'
+    # model = tf.keras.models.load_model(model_path)
 
     model = tf.keras.models.Sequential([
       tf.keras.layers.Flatten(),
@@ -30,4 +35,4 @@ if __name__ == '__main__':
                   metrics=['accuracy'])
     model.fit(x_train, y_train, epochs=10, verbose=1)
     model.evaluate(x_test, y_test)
-    model.save('scp_model.h5')
+    model.save('model/scp_model.h5')
