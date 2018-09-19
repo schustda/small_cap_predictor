7477f548a0aec8e5bcec38d4a37b4485208fa07e
schustda <douglas.schuster@carat.com>
2018-09-19 13:02:06 -0400

    chunksized predictions

--- model/tensorflow_model.py
+++ model/tensorflow_model.py
@@ -1,10 +1,11 @@
-from tensorflow import keras
+import numpy as np
+import pandas as pd
+from time import time
 import tensorflow as tf
 from random import sample
-from model.model_base_class import ModelBaseClass
-from time import time
-import pandas as pd
+from tensorflow import keras
 import matplotlib.pyplot as plt
+from model.model_base_class import ModelBaseClass
 
 
 class TFModel(ModelBaseClass):
@@ -13,28 +14,35 @@ class TFModel(ModelBaseClass):
         if load_data:
             self.load_train_test()
 
-    def add_predictions(self,model):
+    def add_predictions(self,model,shape,chunksize=1000):
 
-        mbc = ModelBaseClass(verbose=1)
-        idxs = mbc.get_list('idxs_to_predict')
-        num_days = mbc.model_params['num_days']
-        mbc.interval_time, mbc.original_time = time(), time()
+        idxs = self.get_list('idxs_to_predict')
+        num_days = self.model_params['num_days']
+        self.interval_time, self.original_time = time(), time()
+        idx_processed = 0
         total = len(idxs)
-        for num,idx in enumerate(idxs):
+        idx_chunks = [idxs[x:x+chunksize] for x in range(0,total,chunksize)]
 
-            replacements = {'{idx}':idx,'{num_days}':num_days}
-            df = mbc.get_df('model_point',replacements=replacements)
-            df = df.sort_values('date')
-            data_point = mbc.transform(df)
-            pred = model.predict_proba(data_point)[0][0]**(1/2)
+        for idx_chunk in idx_chunks:
 
-            update_query = '''
-            UPDATE model.combined_data
-            SET model_development_prediction = {0}
-            WHERE idx = {1}
-            '''.format(pred,idx)
-            mbc.execute_query(update_query)
-            mbc.status_update(num,total)
+            pred_data = np.empty((chunksize,shape))
+            for num,idx in enumerate(idx_chunk):
+                replacements = {'{idx}':idx,'{num_days}':num_days}
+                df = self.get_df('model_point',replacements=replacements)
+                df = df.sort_values('date')
+                pred_data[num] = self.transform(df)
+                idx_processed += 1
+
+            preds = model.predict_proba(pred_data)**(1/self.model_params['target_power'])
+            update_query = ''
+            for idx,pred in zip(idx_chunk,preds):
+                update_query += '''
+                UPDATE model.combined_data
+                SET model_development_prediction = {0}, modified_date = '{1}'
+                WHERE idx = {2};
+                '''.format(pred[0],pd.Timestamp.now(),idx)
+            self.execute_query(update_query)
+            self.status_update(idx_processed,total)
 
     def random_undersampling(self,x,y):
         # there is a high number of points that have
@@ -57,7 +65,7 @@ class TFModel(ModelBaseClass):
             keras.layers.Dense(64, activation=tf.nn.relu,
                        input_shape=(train_data.shape[1],)),
             keras.layers.Dense(64, activation=tf.nn.relu),
-            keras.layers.Dense(1)
+            keras.layers.Dense(1, activation=tf.nn.tanh)
             ])
 
         optimizer = tf.train.RMSPropOptimizer(0.001)
@@ -79,17 +87,17 @@ if __name__ == '__main__':
     train_idx = tfm.train_idx
 
     # x_resampled,y_resampled = tfm.random_undersampling(x_train,y_train)
-    print("Training set: {}".format(x_train.shape))  # 404 examples, 13 features
+    # print("Training set: {}".format(x_train.shape))  # 404 examples, 13 features
 
     # plot_target_hist(y_resampled)
     # plot_target_hist(y_train)
 
-    # model_path = 'model/scp_model.h5'
+    model_path = 'model/scp_model.h5'
     # # model = tf.keras.models.load_model(model_path)
     model = tfm.build_model(x_train)
-    y_train = y_train**2
+    y_train = y_train**(tfm.model_params['target_power'])
     model.fit(x_train, y_train, epochs=20, verbose=1)
-    #
+
     y_test = tfm.y_test
     x_test = tfm.X_test
     test_idx = tfm.test_idx
@@ -97,4 +105,4 @@ if __name__ == '__main__':
     print(model.evaluate(x_test, y_test**2))
     model.save('model/scp_model.h5')
 
-    tfm.add_predictions(model)
+    tfm.add_predictions(model,x_train.shape[1],chunksize=1000)
