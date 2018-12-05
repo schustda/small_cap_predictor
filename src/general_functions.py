695d625befaa235dd7b07b49e51997a8532f9409
schustda <douglas.schuster@carat.com>
2018-12-05 12:59:43 -0500

    cleaned up gf

--- src/general_functions.py
+++ src/general_functions.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 import json
 import psycopg2
 import pandas as pd
@@ -9,56 +10,59 @@ from datetime import datetime
 
 class GeneralFunctions(object):
 
-    def __init__(self, local=False,verbose=0,load_model_params=False):
-        super().__init__()
-        self.local = local
-        json_file = open('src/connection.json')
-        json_str = json_file.read()
-        self.connection = json.loads(json_str)
+    def __init__(self, verbose=0):
+        self.connection = json.loads(open('src/connection.json').read())
         self.verbose = verbose
         self.connect_to_db(self.connection)
 
-    def connect_to_db(self,connection):
+    def verboseprint(self, *args):
+        if self.verbose:
+            for arg in args:
+                print(arg)
+
+    def connect_to_db(self, connection):
         try:
             self.conn = psycopg2.connect(**connection)
             self.cursor = self.conn.cursor()
-            if self.verbose:
-                print('Connected!')
+            self.verboseprint('Connected!')
         except Exception as e:
             print('Not Connected, error: {0}'.format(e))
 
-    def to_table(self,df,table):
+    def to_table(self, df, table):
+        ''' Adds a dataframe in its entirety to a table in the db
 
-        # df = self._reorder_columns(df)
+        Parameters:
+            df (DataFrame): Pandas dataframe to add to the table
+            table (string): Table name to add data to
+        '''
+
+        # self.connect_to_db(self.connection)
         cols = df.columns
         for idx in range(df.shape[0]):
             df_out = df[idx:idx+1]
             output = StringIO()
-            df_out.to_csv(output,index=False,header=False,sep='\t')
+            df_out.to_csv(output, index=False, header=False, sep='\t')
             output.seek(0)
             contents = output.getvalue()
             try:
-                self.cursor.copy_from(output,table,null="",columns=cols)
+                self.cursor.copy_from(output, table, null="", columns=cols)
                 self.conn.commit()
-            except Exception as e :
-                # print (e)
+            except Exception as e:
+                self.verboseprint(e)
                 self.conn.rollback()
+        # self.conn.close()
 
-    def execute_query(self,query):
+    def execute_query(self, query):
         try:
             self.cursor.execute(query)
             self.conn.commit()
-            # if self.verbose:
-            #     print(query)
-        except Exception as e :
-            if self.verbose:
-                print(e)
+            self.verboseprint(query)
+        except Exception as e:
+            self.verboseprint(e)
             self.conn.rollback()
 
-
-    def _format_query(self,query_input,replacements={}):
-        '''
-        Takes in a string or .sql file and optional 'replacements' dictionary.
+    def _format_query(self, query_input, replacements={}):
+        ''' Takes in a string or .sql file and optional 'replacements' dictionary.
 
         Returns a string containing the formatted sql query and replaces the
         keys in the replacements dictionary with their values.
@@ -66,41 +70,58 @@ class GeneralFunctions(object):
 
         # checks if input is a file or query
         if query_input.split('.')[-1] == 'sql':
-            # print('Reading .sql File')
-            f = open(query_input,'r')
-            # reading files with a guillemet », add an uncessary Â to the string
-            query = f.read().replace('Â','')
+            f = open(query_input, 'r')
+            query = f.read().replace('Â', '')
             f.close()
         else:
             query = query_input
         if replacements:
-            for key,value in replacements.items():
-                query = query.replace(key,str(value))
+            for key, value in replacements.items():
+                query = query.replace(key, str(value))
         return query
 
-    def get_value(self,query_input,symbol_id=None,replacements={}):
+    def _get(self, get_type, query_input, symbol_id, replacements):
+        ''' Method used by other functions to generate and execute a query'''
+
         replacements['{symbol_id}'] = symbol_id
-        query_input = 'queries/get_value/{0}.sql'.format(query_input)
-        query = self._format_query(query_input,replacements)
+        query_input = f'queries/{get_type}/{query_input}.sql'
+        query = self._format_query(query_input, replacements)
         self.cursor.execute(query)
+
+    def get_value(self, query_input, symbol_id=None, replacements={}):
+        '''Returns a value from the database
+
+        Parameters:
+            query_input (str): Name of the file located in ./queries/get_value
+            symbol_id (int): If a symbol_id is needed, put it here
+            replacements (dict): Replace key with value in the query
+        '''
+        self._get('get_value', query_input, symbol_id, replacements)
         return self.cursor.fetchone()[0]
 
-    def get_list(self,query_input,symbol_id=None,replacements={}):
-        replacements['{symbol_id}'] = symbol_id
-        query_input = 'queries/get_list/{0}.sql'.format(query_input)
-        query = self._format_query(query_input,replacements)
-        self.cursor.execute(query)
-        output = self.cursor.fetchall()
-        return [x[0] for x in output]
+    def get_list(self, query_input, symbol_id=None, replacements={}):
+        '''Returns a list from the database
 
-    def get_dict(self,query_input,symbol_id=None,replacements={}):
-        replacements['{symbol_id}'] = symbol_id
-        query_input = 'queries/get_dict/{0}.sql'.format(query_input)
-        query = self._format_query(query_input,replacements)
-        self.cursor.execute(query)
+        Parameters:
+            query_input (str): Name of the file located in ./queries/get_list
+            symbol_id (int): If a symbol_id is needed, put it here
+            replacements (dict): Replace key with value in the query
+        '''
+        self._get('get_list', query_input, symbol_id, replacements)
+        return [x[0] for x in self.cursor.fetchall()]
+
+    def get_dict(self, query_input, symbol_id=None, replacements={}):
+        '''Returns a dict from the database. Specify two columns in the query
+
+        Parameters:
+        query_input (str): Name of the file located in ./queries/get_dict
+        symbol_id (int): If a symbol_id is needed, put it here
+        replacements (dict): Replace key with value in the query
+        '''
+        self._get('get_dict', query_input, symbol_id, replacements)
         return dict(self.cursor.fetchall())
 
-    def get_df(self,query_input,symbol_id=None,replacements={}):
+    def get_df(self, query_input, symbol_id=None, replacements={}):
         '''
         Takes in a string containing either a correctly formatted SQL
         query, or filepath directed to a .sql file. Returns a pandas DataFrame
@@ -112,11 +133,11 @@ class GeneralFunctions(object):
             'model_data','model_point']:
             query_input = 'queries/get_df/{0}.sql'.format(query_input)
         query = self._format_query(query_input,replacements)
-        # if self.verbose:
-        #     print ('Executing Query:\n\n',format(query,reindent=True,keyword_case='upper'))
         return pd.read_sql(query,self.conn)
 
     def list_tables(self):
+        '''Print a list of user-created tables within the db'''
+
         self.cursor.execute("""SELECT CONCAT(table_schema,'.',table_name) AS tables
             FROM information_schema.tables
             WHERE table_schema != 'pg_catalog'
@@ -125,24 +146,26 @@ class GeneralFunctions(object):
         for table in self.cursor.fetchall():
             print(table[0])
 
-    def status_update(self,num,total):
-        '''
-        Provides an update every minute on the progress of a given function
-        '''
-        try:
-            percent = round(num / total * 100,2)
-        except:
-            percent = 0
-        # Display update ever 60 seconds
+    def status_update(self, num, total):
+        '''Prints an update every minute on the progress'''
+
+        if not total:
+            return
         if time() > self.interval_time + 60:
-            time_elapsed = time() - self.original_time
-            a = int(percent/2)
-            b = 50-a
-            if percent == 0:
-                percent = 0.5
-            min_rem = int(time_elapsed/percent*(100-percent)/60)
-            print ('|{0}{1}| {2}% - {3} minute(s) remaining {4}/{5}'.format(a*'=',b*'-',str(percent),str(min_rem),num,total))
+            elapsed_time = (time() - self.original_time) / 60
+            percent = round(num / total * 100, 2)
+            if not percent:
+                percent = num / total
+            a = int(percent / 2) * '='
+            b = (50 - int(percent / 2)) * '-'
+            min_rem = str(int((elapsed_time) / percent * (100 - percent)))
+            print('\r', f'|{a}{b}| {num}/{total} ({percent:0.2f}%) - {min_rem} minute(s) remaining')
             self.interval_time = time()
 
 if __name__ == '__main__':
     gf = GeneralFunctions(verbose=1)
+    # gf.list_tables()
+    gf.interval_time, gf.original_time = time(), time()
+    tot = range(1000000000000)
+    for i in tot:
+        gf.status_update(i,len(tot))
