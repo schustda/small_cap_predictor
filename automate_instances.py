b644e971c4591dd3f02ae4a5156364e36dbe2cdf
schustda <douglas.schuster@carat.com>
2019-03-19 18:54:53 -0400

    automate instances

---
+++ automate_instances.py
@@ -0,0 +1,94 @@
+from googleapiclient import discovery
+from datetime import datetime
+
+def delete_instance(compute, project, zone, name):
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
+
+def create_instance(compute, project, zone, name):
+    # Get the latest Debian Jessie image.
+    source_disk_image = 'https://www.googleapis.com/compute/v1/projects/us-gm-175021/global/images/scp'
+    machine_type = "zones/{0}/machineTypes/f1-micro".format(zone)
+    startup_script = 'gs://scp_ds/boot_up.sh'
+
+    # Configure the machine
+    # machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+    # image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+    # image_caption = "Ready for dessert?"
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'https://www.googleapis.com/compute/v1/projects/us-gm-175021/global/networks/default',
+            'subnetwork': 'https://www.googleapis.com/compute/v1/projects/us-gm-175021/regions/us-east1/subnetworks/default',
+            'name': 'nic0',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'external-nat', 'networkTier': 'PREMIUM'}
+            ]
+        }],
+        #
+        # # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'startup-script-url',
+                'value': startup_script
+            }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+
+if __name__ == '__main__':
+    compute = discovery.build('compute', 'v1')
+    project = 'us-gm-175021'
+    zone = 'us-east1-b'
+
+    max_num_instances = 1000
+    instance_limit = 60*60*24*3
+    sleep_mins = 20
+    instance_num = 1
+    instances = {}
+    while True:
+
+        if len(instances) < max_num_instances:
+            instance_name = 'scp-{:06d}'.format(instance_num)
+            create_instance(compute, project, zone, instance_name)
+            instances[instance_name] = datetime.now()
+
+        for name, created_date in instances.items():
+                if (datetime.now() - created_date).total_seconds() > instance_limit:
+                delete_instance(compute, project, zone, name)
+
+        sleep(60*sleep_mins)
--- etl/ihub_sentiment.py
+++ etl/ihub_sentiment.py
@@ -82,7 +82,7 @@ class IhubSentiment(GeneralFunctions):
         try:
             df = self.get_message_page(message_id)
             self.to_table(df,'ihub.message_sentiment')
-            sleep(randint(3, 10))
+            sleep(randint(2, 6))
         except Exception as e:
             print(e)
             sleep(60)
